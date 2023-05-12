ab0f50c1f19c5de4993dfff870e2e2068e2a7182
Maksim D <d.maks.1883@gmail.com>
2023-05-12 18:17:37 +0300

    initial commit

---
+++ src/test/java/ru/netology/OrderCardDeliveryTest.java
@@ -0,0 +1,121 @@
+package ru.netology;
+
+import org.junit.jupiter.api.*;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class OrderCardDeliveryTest {
+    public static String setLocalDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy",
+                new Locale("ru")));
+    }
+
+    @BeforeEach
+    void setupTest() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldSubmitValidData3() {
+        String date = setLocalDate(3);
+        $("[data-test-id=city] .input__control").setValue("Казань");
+        $("[data-test-id = date] .input__control").doubleClick().sendKeys(date);
+        $("[data-test-id = name] .input__control").setValue("Иван Петров");
+        $("[data-test-id = phone] .input__control").setValue("+79112345678");
+        $("[data-test-id = agreement]").click();
+        $(".button").click();
+        $("[data-test-id = notification]").shouldHave(text("Успешно!"),
+                Duration.ofSeconds(15)).shouldBe(visible);
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                Duration.ofSeconds(15)).shouldBe(visible);
+    }
+
+    @Test
+    void shouldSubmitValidData4() {
+        String date = setLocalDate(4);
+        $("[data-test-id=city] .input__control").setValue("Казань");
+        $("[data-test-id = date] .input__control").doubleClick().sendKeys(date);
+        $("[data-test-id = name] .input__control").setValue("Иван Петров");
+        $("[data-test-id = phone] .input__control").setValue("+79112345678");
+        $("[data-test-id = agreement]").click();
+        $(".button").click();
+        $("[data-test-id = notification]").shouldHave(text("Успешно!"),
+                Duration.ofSeconds(15)).shouldBe(visible);
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                Duration.ofSeconds(15)).shouldBe(visible);
+    }
+
+    @Test
+    void shouldSubmitValidDataYear() {
+        String date = setLocalDate(365);
+        $("[data-test-id=city] .input__control").setValue("Казань");
+        $("[data-test-id = date] .input__control").doubleClick().sendKeys(date);
+        $("[data-test-id = name] .input__control").setValue("Иван Петров");
+        $("[data-test-id = phone] .input__control").setValue("+79112345678");
+        $("[data-test-id = agreement]").click();
+        $(".button").click();
+        $("[data-test-id = notification]").shouldHave(text("Успешно!"),
+                Duration.ofSeconds(15)).shouldBe(visible);
+        $(".notification__content").shouldHave(text("Встреча успешно забронирована на " + date),
+                Duration.ofSeconds(15)).shouldBe(visible);
+    }
+    @Test
+    void shouldSubmitNotValidData2() {
+        String date = setLocalDate(2);
+        $("[data-test-id=city] .input__control").setValue("Казань");
+        $("[data-test-id = date] .input__control").doubleClick().sendKeys(date);
+        $("[data-test-id = name] .input__control").setValue("Иван Петров");
+        $("[data-test-id = phone] .input__control").setValue("+79112345678");
+        $("[data-test-id = agreement]").click();
+        $(".button").click();
+        $(withText("невозможен")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(10));
+    }
+
+    @Test
+    void shouldSubmitNotValidDataToday() {
+        String date = setLocalDate(0);
+        $("[data-test-id=city] .input__control").setValue("Казань");
+        $("[data-test-id = date] .input__control").doubleClick().sendKeys(date);
+        $("[data-test-id = name] .input__control").setValue("Иван Петров");
+        $("[data-test-id = phone] .input__control").setValue("+79112345678");
+        $("[data-test-id = agreement]").click();
+        $(".button").click();
+        $(withText("невозможен")).should(visible, Duration.ofSeconds(5));
+        $x("//*[@data-test-id=\"notification\"]").shouldNot(visible, Duration.ofSeconds(10));
+    }
+
+    @Test
+    void shouldSubmitNotValidCity() {
+        String date = setLocalDate(3);
+        $("[data-test-id=city] .input__control").setValue("Купчино");
+        $("[data-test-id = date] .input__control").doubleClick().sendKeys(date);
+        $("[data-test-id = name] .input__control").setValue("Иван Петров");
+        $("[data-test-id = phone] .input__control").setValue("+79112345678");
+        $("[data-test-id = agreement]").click();
+        $(".button").click();
+        $("[data-test-id='city'].input_invalid").shouldBe(visible, Duration.ofSeconds(5))
+                .should(exactText("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void shouldSubmitNotValidCityEnglish() {
+        String date = setLocalDate(3);
+        $("[data-test-id=city] .input__control").setValue("Kazan");
+        $("[data-test-id = date] .input__control").doubleClick().sendKeys(date);
+        $("[data-test-id = name] .input__control").setValue("Ирина Ким");
+        $("[data-test-id = phone] .input__control").setValue("+79600000000");
+        $("[data-test-id = agreement]").click();
+        $(".button").click();
+        $("[data-test-id='city'].input_invalid").shouldBe(visible, Duration.ofSeconds(5))
+                .should(exactText("Доставка в выбранный город недоступна"));
+    }
+// все остальные тесты для ФИО, телефона аналогично
+}
